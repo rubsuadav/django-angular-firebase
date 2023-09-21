75c54ee0a8e6f30c4775017ef768b49962f1dbae
rubsuadav <rsuarezdavid@gmail.com>
2023-09-21 18:51:00 +0200

    added authorizacion on the page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'Sistema de Posts';
+export class AppComponent implements OnInit {
+  showRegisterButton: boolean = true;
+  showLoginButton: boolean = true;
+  showCreatePostButton: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.showRegisterButton = false;
+    }
+    if (token && !this.showRegisterButton) {
+      this.showLoginButton = false;
+    }
+    if (token && !this.showRegisterButton && !this.showLoginButton) {
+      this.showCreatePostButton = true;
+    }
+  }
 }
